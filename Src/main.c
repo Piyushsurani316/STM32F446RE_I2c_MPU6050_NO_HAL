858c59de2c0e2291e11e748470023bd75fb6d61e
PIYUSH SURANI <surani.piyush316@gmail.com>
2026-02-13 21:33:36 +0000

    second commit

---
+++ Src/main.c
@@ -0,0 +1,222 @@
+
+#include <stm32f446xx.h>
+void MPU6050_Init (void);
+void I2C_Config (void);
+
+void I2C_Start (void);
+
+void I2C_Write (uint8_t data);
+
+void I2C_Address (uint8_t Address);
+
+void I2C_Stop (void);
+
+void I2C_Read (uint8_t Address, uint8_t *buffer, uint8_t size);
+
+void MPU_Write (uint8_t Address, uint8_t Reg, uint8_t Data)
+{
+	I2C_Start ();
+	I2C_Address (Address);
+	I2C_Write (Reg);
+	I2C_Write (Data);
+	I2C_Stop ();
+}
+
+void MPU_Read (uint8_t Address, uint8_t Reg, uint8_t *buffer, uint8_t size)
+{
+	I2C_Start ();
+	I2C_Address (Address);
+	I2C_Write (Reg);
+	I2C_Start ();  // repeated start
+	I2C_Read (Address+0x01, buffer, size);
+	I2C_Stop ();
+}
+
+
+#define MPU6050_ADDR 0xD0
+
+
+#define SMPLRT_DIV_REG 0x19
+#define GYRO_CONFIG_REG 0x1B
+#define ACCEL_CONFIG_REG 0x1C
+#define ACCEL_XOUT_H_REG 0x3B
+#define TEMP_OUT_H_REG 0x41
+#define GYRO_XOUT_H_REG 0x43
+#define PWR_MGMT_1_REG 0x6B
+#define WHO_AM_I_REG 0x75
+
+
+int16_t Accel_X_RAW = 0;
+int16_t Accel_Y_RAW = 0;
+int16_t Accel_Z_RAW = 0;
+
+int16_t Gyro_X_RAW = 0;
+int16_t Gyro_Y_RAW = 0;
+int16_t Gyro_Z_RAW = 0;
+
+float Ax, Ay, Az, Gx, Gy, Gz;
+
+uint8_t check;
+
+void MPU6050_Init (void)
+{
+	uint8_t check;
+	uint8_t Data;
+
+	// check device ID WHO_AM_I
+
+	MPU_Read (MPU6050_ADDR,WHO_AM_I_REG, &check, 1);
+
+	if (check == 104)  // 0x68 will be returned by the sensor if everything goes well
+	{
+		// power management register 0X6B we should write all 0's to wake the sensor up
+		Data = 0;
+		MPU_Write (MPU6050_ADDR, PWR_MGMT_1_REG, Data);
+
+		// Set DATA RATE of 1KHz by writing SMPLRT_DIV register
+		Data = 0x07;
+		MPU_Write(MPU6050_ADDR, SMPLRT_DIV_REG, Data);
+
+		// Set accelerometer configuration in ACCEL_CONFIG Register
+		// XA_ST=0,YA_ST=0,ZA_ST=0, FS_SEL=0 -> ? 2g
+		Data = 0x00;
+		MPU_Write(MPU6050_ADDR, ACCEL_CONFIG_REG, Data);
+
+		// Set Gyroscopic configuration in GYRO_CONFIG Register
+		// XG_ST=0,YG_ST=0,ZG_ST=0, FS_SEL=0 -> ? 250 ?/s
+		Data = 0x00;
+		MPU_Write(MPU6050_ADDR, GYRO_CONFIG_REG, Data);
+	}
+
+}
+
+void MPU6050_Read_Accel (void)
+{
+	
+	uint8_t Rx_data[6];
+	
+	// Read 6 BYTES of data starting from ACCEL_XOUT_H register
+
+	MPU_Read (MPU6050_ADDR, ACCEL_XOUT_H_REG, Rx_data, 6);
+
+	Accel_X_RAW = (int16_t)(Rx_data[0] << 8 | Rx_data [1]);
+	Accel_Y_RAW = (int16_t)(Rx_data[2] << 8 | Rx_data [3]);
+	Accel_Z_RAW = (int16_t)(Rx_data[4] << 8 | Rx_data [5]);
+
+	/*** convert the RAW values into acceleration in 'g'
+	     we have to divide according to the Full scale value set in FS_SEL
+	     I have configured FS_SEL = 0. So I am dividing by 16384.0
+	     for more details check ACCEL_CONFIG Register              ****/
+
+	Ax = Accel_X_RAW/16384.0;
+	Ay = Accel_Y_RAW/16384.0;
+	Az = Accel_Z_RAW/16384.0;
+}
+void TIM6Config (void)
+{
+	/************** STEPS TO FOLLOW *****************
+	1. Enable Timer clock
+	2. Set the prescalar and the ARR
+	3. Enable the Timer, and wait for the update Flag to set
+	************************************************/
+
+// 1. Enable Timer clock
+	RCC->APB1ENR |= (1<<4);  // Enable the timer6 clock
+
+// 2. Set the prescalar and the ARR
+	TIM6->PSC = 90-1;  // 90MHz/90 = 1 MHz ~~ 1 uS delay
+	TIM6->ARR = 0xffff;  // MAX ARR value
+
+// 3. Enable the Timer, and wait for the update Flag to set
+	TIM6->CR1 |= (1<<0); // Enable the Counter
+	while (!(TIM6->SR & (1<<0)));  // UIF: Update interrupt flag..  This bit is set by hardware when the registers are updated
+}
+
+void Delay_us (uint16_t us)
+{
+	/************** STEPS TO FOLLOW *****************
+	1. RESET the Counter
+	2. Wait for the Counter to reach the entered value. As each count will take 1 us,
+		 the total waiting time will be the required us delay
+	************************************************/
+	TIM6->CNT = 0;
+	while (TIM6->CNT < us);
+}
+
+void Delay_ms (uint16_t ms)
+{
+	for (uint16_t i=0; i<ms; i++)
+	{
+		Delay_us (1000); // delay of 1 ms
+	}
+}
+
+void SysClockConfig (void)
+{
+		/*************>>>>>>> STEPS FOLLOWED <<<<<<<<************
+
+	1. ENABLE HSE and wait for the HSE to become Ready
+	2. Set the POWER ENABLE CLOCK and VOLTAGE REGULATOR
+	3. Configure the FLASH PREFETCH and the LATENCY Related Settings
+	4. Configure the PRESCALARS HCLK, PCLK1, PCLK2
+	5. Configure the MAIN PLL
+	6. Enable the PLL and wait for it to become ready
+	7. Select the Clock Source and wait for it to be set
+
+	********************************************************/
+
+
+	#define PLL_M 	4
+	#define PLL_N 	180
+	#define PLL_P 	0  // PLLP = 2
+
+	// 1. ENABLE HSE and wait for the HSE to become Ready
+	RCC->CR |= RCC_CR_HSEON;
+	while (!(RCC->CR & RCC_CR_HSERDY));
+
+	// 2. Set the POWER ENABLE CLOCK and VOLTAGE REGULATOR
+	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+	PWR->CR |= PWR_CR_VOS;
+
+
+	// 3. Configure the FLASH PREFETCH and the LATENCY Related Settings
+	FLASH->ACR = FLASH_ACR_ICEN | FLASH_ACR_DCEN | FLASH_ACR_PRFTEN | FLASH_ACR_LATENCY_5WS;
+
+	// 4. Configure the PRESCALARS HCLK, PCLK1, PCLK2
+	// AHB PR
+	RCC->CFGR |= RCC_CFGR_HPRE_DIV1;
+
+	// APB1 PR
+	RCC->CFGR |= RCC_CFGR_PPRE1_DIV4;
+
+	// APB2 PR
+	RCC->CFGR |= RCC_CFGR_PPRE2_DIV2;
+
+
+	// 5. Configure the MAIN PLL
+	RCC->PLLCFGR = (PLL_M <<0) | (PLL_N << 6) | (PLL_P <<16) | (RCC_PLLCFGR_PLLSRC_HSE);
+
+	// 6. Enable the PLL and wait for it to become ready
+	RCC->CR |= RCC_CR_PLLON;
+	while (!(RCC->CR & RCC_CR_PLLRDY));
+
+	// 7. Select the Clock Source and wait for it to be set
+	RCC->CFGR |= RCC_CFGR_SW_PLL;
+	while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
+}
+
+
+int main ()
+{
+	SysClockConfig ();
+	TIM6Config ();
+	I2C_Config ();
+
+	MPU6050_Init ();
+	while (1)
+	{
+		MPU6050_Read_Accel ();
+		Delay_ms (1000);
+	}
+}
+
